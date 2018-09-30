e3610c6044b1400e86d4424a91532026d49462f5
jiangcw <jiangcw@Ctrip.com>
2018-09-30 15:10:29 +0800

    del

--- src/main/java/com/langel/lavcache/scanner/Scanner.java
+++
@@ -1,9 +0,0 @@
-package com.langel.lavcache.scanner;
-
-/**
- * @author rick(lonelyangel.jcw @ gamil.com)
- * @date 2018/9/23 下午8:25
- **/
-public interface Scanner {
-    void scan(String basePackgaeName);
-}
